7a0e0b07d006f778ae2218fe1502a75193c1e217
Bastien Sarolea <bastien.sarolea@gmail.com>
2025-06-17 16:26:19 +0200

    05

--- docs/lensID.js
+++ docs/lensID.js
@@ -23,9 +23,9 @@ function getURLorFallback(key, defaultValue) {
 window.lensID = getURLorFallback('lensID', '96a1bc1f-f39a-44e5-aa85-b337029d6093'); // DG-web
 window.groupID = getURLorFallback('groupID', 'f7f4e367-f4b3-4de5-8e81-e9c842f2bf0b'); // LIVE_PROD Group
 window.modePhoto = getURLorFallback('modePhoto', true);
-window.modeVideo = getURLorFallback('modeVideo', true);
-window.modeBothCamera = getURLorFallback('modeBothCamera', true);
-window.modeStartFaceCamera = getURLorFallback('modeStartFaceCamera', false);
+window.modeVideo = getURLorFallback('modeVideo', false);
+window.modeBothCamera = getURLorFallback('modeBothCamera', false);
+window.modeStartFaceCamera = getURLorFallback('modeStartFaceCamera', true);
 window.splashScreen = getURLorFallback('splashScreen', undefined);
 
 
