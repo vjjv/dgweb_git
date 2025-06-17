64314afbb2b19ecf15b723b8a898e95c1dcddb65
Bastien Sarolea <bastien.sarolea@gmail.com>
2025-06-17 11:43:01 +0200

    02

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ const captureRenderTarget = document.getElementById('capture-canvas');
 const flipCamera = document.getElementById('flip');
 const intro = document.getElementById('intro-bg');
 var firstTime = true;
-if(window.splashScreen){
+if (window.splashScreen) {
     document.body.addEventListener('click', () => {
         if (firstTime) {
             firstTime = false;
@@ -22,8 +22,8 @@ if(window.splashScreen){
             init();
         }
     }, true);
-    
-}else{
+
+} else {
     intro.style.display = 'none';
     init();
 }
@@ -36,7 +36,7 @@ async function init() {
         // apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNjk4MDU3NzAyLCJzdWIiOiI0MDUyY2RlNC02YzMzLTRkM2UtYTJjNC0yNzllYzc1M2VmOWR-U1RBR0lOR341MTY4YzVmNC1kYWVkLTQ1N2ItOGJmYy01Y2JhODkwOWU4OTgifQ.b0Z-TegYa2Sg-lZy_8XoPw7f_iz7eEC5BtzYooyL5K4',
         apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzM4MjM2Njg5LCJzdWIiOiJmYWMzYWZjOS0zOTEyLTRlNTUtYTdiZS03MjJlOGRmYWY4ZjV-UFJPRFVDVElPTn5lOGQ0OTM1NS00YmNlLTRiYWEtODkzNC1lMWNlNmU0ZDM5M2IifQ.6sZB_6aFPL8OW-UO3Y37P7Rev7mzjS9IhNRFk7NelBI',
     });
-    
+
     const session = await cameraKit.createSession({ liveRenderTarget });
     captureRenderTarget.replaceWith(session.output.capture);
 
@@ -79,6 +79,8 @@ async function updateCamera(session) {
 
     mediaStream = await navigator.mediaDevices.getUserMedia({
         video: {
+            width: { ideal: 1280 },
+            height: { ideal: 720 },
             facingMode: isBackFacing ? 'environment' : 'user',
         },
     });
@@ -97,10 +99,10 @@ async function updateCamera(session) {
     const height = window.innerHeight * resolutionMultiplier;
     source.setRenderSize(width, height);
     console.log('Pixelate Debug')
-    console.log('window.devicePixelRatio : '+window.devicePixelRatio)
-    console.log('window.innerWidth : '+window.innerWidth)
-    console.log('window.innerHeight : '+window.innerHeight)
-    console.log('source.setRenderSize(width, height) : '+window.innerWidth * resolutionMultiplier+ ', '+window.innerHeight * resolutionMultiplier)
+    console.log('window.devicePixelRatio : ' + window.devicePixelRatio)
+    console.log('window.innerWidth : ' + window.innerWidth)
+    console.log('window.innerHeight : ' + window.innerHeight)
+    console.log('source.setRenderSize(width, height) : ' + window.innerWidth * resolutionMultiplier + ', ' + window.innerHeight * resolutionMultiplier)
     //END DEBUG
 
     if (!isBackFacing) {
